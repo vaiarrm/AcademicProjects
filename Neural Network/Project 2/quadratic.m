96ed5ccadbea0df3a6e267dbd05b8253e99c08a6
Vaibhav Sharma <vaibhav.s.sharma@outlook.com>
2016-10-23 20:14:50 -0500

    UPDATED VERSION EXCLUDING STOPPING

--- Neural Network/Project 2/quadratic.m
+++ Neural Network/Project 2/quadratic.m
@@ -1,1 +1,1 @@
-###author: Vaibhav Sharma#Date: 10/22/2016#Description: Calculates the Quadratic Cost#Output: Quadratic cost##function qCost = quadratic(t,a,lambdaReg)  qCost = (sum(sum((t - a).^2,2))) / 2;  qCost = qCost + + sumOfWeightsSquared(lambdaReg);endfunction
+###author: Vaibhav Sharma#Date: 10/22/2016#Description: Calculates the Quadratic Cost#Output: Quadratic cost##function qCost = quadratic(t,a,lambdaReg)  qCost = (sum(sum((t - a).^2,2))) / 2;  qCost = qCost +  sumOfWeightsSquared(lambdaReg);endfunction
